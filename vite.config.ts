d477bd9b4dcc3d91b63047d2b4918c7511e01405
Horacio M. Lerma <horacio@horacio.one>
2025-07-23 15:31:09 +0200

    afegint dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',  // <-- cambia esto para que las rutas sean relativas
+  base: './',  
   server: {
     host: "::",
     port: 8080,
