c7bc0027be84487631d66cbc5c9c59ea32330bb8
Horacio M. Lerma <horacio@horacio.one>
2025-07-23 15:29:04 +0200

    desplegar pàgina web

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './',  // <-- cambia esto para que las rutas sean relativas
   server: {
     host: "::",
     port: 8080,
